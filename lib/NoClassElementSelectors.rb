bdd171fb781d4128ddd47d55e55130063b0b4987
Shukri Adams <shukri.adams@gmail.com>
2016-07-04 17:03:22 +0200

    Fixed handling of child elements.

--- lib/NoClassElementSelectors.rb
+++ lib/NoClassElementSelectors.rb
@@ -9,29 +9,39 @@ module SCSSLint
                  @ignoredElements = config['ignoredElements']
             end
 
+            @depth = 1
+            @root = ""
+
             yield # Continue linting children
         end
 
+
         def visit_rule(node)
+
             selectors = node.parsed_rules.to_s.split(" ")
-            hasClass = false
-            hasElement = false
+            isViolatingElement = false
+
             selectors.each do |selector|
-                if selector.start_with?('.')
-                    hasClass  = true
+
+                if @depth == 1
+                    @root = selector
                 end
 
-                if !selector.start_with?('.') and !@ignoredElements.include?(selector)
-                    hasElement = true
-                end
-
-                if hasClass and hasElement
-                    add_lint(node, "Avoid selectors that combine classes and elements - give your elements unique class names instead.")
-                    break
+                if !selector.start_with?('.') and !selector.start_with?('&') and !@ignoredElements.include?(selector)
+                    isViolatingElement = true
                 end
 
             end
 
+            if @depth > 1 and @root.start_with?('.') and isViolatingElement
+                add_lint(node, "Avoid selectors that combine classes and elements - give your elements unique class names instead.")
+            else
+                @depth += 1
+                yield # Continue linting children
+                @depth -= 1
+            end
+
+            #yield # Continue linting children
         end
 
     end
